36e1767978ce1c90e40aaf91ffde87892d189496
Vadim Andrushenko <vadimandrusenko320@gmail.com>
2025-09-13 23:17:32 +0700

    first commint

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index2.html",
+    "./src/**/*.{html,js}" ,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
