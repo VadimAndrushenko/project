3a93b1d23f973a974cbe8dcce01188ad3eba3f42
Vadim Andrushenko <vadimandrusenko320@gmail.com>
2025-09-17 01:25:02 +0700

    Keep local state

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,24 @@
 export default {
   content: [
     "./index.html",
+    "./index-shop.html",
+    "./index-card.html",
+    "./index-aliquam-furniture.html",
     "./src/**/*.{html,js}" ,
   ],
   theme: {
 
     container: {
       center: true,
-      padding: "2rem",
-      
+      padding: {
+        DEFAULT: '1rem', 
+        sm: '2rem', 
+      },
     },
 
-    // extend: {
+    extend: {
 
-    // },
+    },
   },
   plugins: [],
 }
